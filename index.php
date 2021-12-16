6362c98d71656e5d86f64d5cb94688ef7e4c099b
Ramesh KHARAT <62638281+kharatramesh@users.noreply.github.com>
2021-12-16 11:05:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo " - Name: " . $row["firstname"]. " " . $row["rollnumber"]. "<br>";
+    echo " - Student Name: " . $row["firstname"]. " " . $row["rollnumber"]. "<br>";
   }
 } else {
   echo "0 results";
